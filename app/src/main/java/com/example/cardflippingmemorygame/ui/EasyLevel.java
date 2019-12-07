8033a774aed500a94b92024aeda152c632b61586
sat0004 <satyam13arora1999@gmail.com>
2019-12-07 18:32:14 +0530

    Added layout design for StartingActivity

--- app/src/main/java/com/example/cardflippingmemorygame/ui/EasyLevel.java
+++ app/src/main/java/com/example/cardflippingmemorygame/ui/EasyLevel.java
@@ -1,14 +1,16 @@
-package com.example.cardflippingmemorygame;
+package com.example.cardflippingmemorygame.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.cardflippingmemorygame.R;
+
+public class EasyLevel extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_easy_level);
     }
 }
